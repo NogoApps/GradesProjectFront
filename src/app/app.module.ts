a543d1261e08e3a94707f000c653ccdfd7019a59
GorickUPM <danilosilvaweb@hotmail.com>
2017-02-03 17:37:59 -0300

    afirst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PageIndexComponent } from './page-index/page-index.component';
+import { DialogLoginComponent } from './dialog-login/dialog-login.component';
+import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PageIndexComponent,
+    DialogLoginComponent,
+    DropdownMenuComponent
   ],
   imports: [
     BrowserModule,
